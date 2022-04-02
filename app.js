d31d9b2ee9e56f50c9396217d2e2af6506d0a1ba
unknown <brandonqiu777@gmail.com>
2022-04-02 18:28:15 -0400

    bugfix

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 280db0e6bea3dc2c8a71fb4eeee9df94324ad43a
-
 setTimeout(() => { let canvas = document.getElementById("myCanvas");
 console.log(canvas)
-const redButton = document.getElementById("red");
-
 
 let ctx = canvas.getContext("2d");
 
@@ -16,23 +9,6 @@ document.addEventListener("mousedown", start);
 document.addEventListener("mouseup", stop);
 window.addEventListener("resize", resize);
 
-function colorPicker(col){
-    if (col == "red"){
-        return "#FF0000"
-    }
-    else if (col == "blue"){
-        return "#0000FF"
-    }
-    else if (col == "green"){
-        return "#008000"
-    }
-    else if (col == "black"){
-        return "#000000"
-    }
-    else{
-        return
-    }
-}
 
 function resize() {
   ctx.canvas.width = window.innerWidth - 545;
@@ -50,10 +26,7 @@ function stop() {
   document.removeEventListener("mousemove", draw);
 }
 function draw(event, color) {
-    redButton.addEventListener("click", () =>{
-        color = "#FF0000"
-});
-    ctx.beginPath();
+ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
   if (color == null){
@@ -66,23 +39,3 @@ function draw(event, color) {
   ctx.stroke();
 }
  }, 2000);
-
-<<<<<<< HEAD
-// =======
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-// app.get('/hi', (req, res) => {
-//     res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-// >>>>>>> 9b02b8b5b56da5774d4ce5114299594a6cadd2d9
-=======
->>>>>>> 280db0e6bea3dc2c8a71fb4eeee9df94324ad43a
