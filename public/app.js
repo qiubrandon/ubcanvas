ab8ae92b57f73baff080a3b70bfc395b24ef4694
unknown <brandonqiu777@gmail.com>
2022-04-04 20:38:51 -0400

    updated server.js

--- public/app.js
+++ public/app.js
@@ -1,40 +1,83 @@
-setTimeout(() => { 
-  let canvas = document.getElementById("myCanvas")
-  console.log(canvas)
+//const { Socket } = require("engine.io");
+function word(){
+  let wordlist = "plate trash paper plane loops brick water jeans strip fires roman grape vault amber nanny scalp drift piles scalp final trace quite quiet dense cocky based baggy click feint these puffy diver slept speed fetus pound gland globe teach"
+  wordlist = wordlist.split(" ");
+  let randomNum = Math.floor(Math.random() * wordlist.length);
+  let word = wordlist[randomNum];
+  return word;
+}
 
-  let ctx = canvas.getContext("2d");
+window.addEventListener("load", () => {
+  
+  const randomize = document.getElementById("randomizeBtn");
+  
+  const wordDisplay = document.getElementById("word");
+  
+  const red = document.getElementById("red")
+  const blue = document.getElementById("blue")
+  const black = document.getElementById("black")
+  red.addEventListener("click", ()=>{
+    document.getElementById("colour").innerHTML = "red"
+    
+  })
+  blue.addEventListener("click", ()=>{
+    document.getElementById("colour").innerHTML = "blue"
+    
+  })
+  black.addEventListener("click", ()=>{
+    document.getElementById("colour").innerHTML = "black"
+    
+  })
 
+
+
+
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
   let coord = { x: 0, y: 0 };
-
+  resize()
   document.addEventListener("mousedown", start);
   document.addEventListener("mouseup", stop);
   window.addEventListener("resize", resize);
+  randomize.addEventListener("click", rando);
+  
+  rando()
 
-  resize()
-
-  function resize() {
+  function resize(){
     ctx.canvas.width = window.innerWidth * .5;
     ctx.canvas.height = window.innerHeight * .7;
   }
+  
   function reposition(event) {
     coord.x = (event.clientX - canvas.offsetLeft) - 16;
     coord.y = (event.clientY - canvas.offsetTop) + 20;
+    const data = {x: coord.x, y: coord.y}
+    
   }
+  
+
   function start(event) {
     document.addEventListener("mousemove", draw);
     reposition(event);
+  
   }
   function stop() {
     document.removeEventListener("mousemove", draw);
   }
-  function draw(event, color) {
+  
+  function draw(event) {
+ 
   ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.lineCap = "round";
-    ctx.strokeStyle = "#000000";
+    ctx.strokeStyle = document.getElementById("colour").innerHTML;
     ctx.moveTo(coord.x, coord.y);
     reposition(event);
     ctx.lineTo(coord.x, coord.y);
     ctx.stroke();
   }
-}, 100);
+  function rando(){
+    wordDisplay.innerHTML = "Try drawing this: " + word();
+  }
+  
+})
--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+const socket = require('socket.io')
+const fs = require('fs');
 
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
 
-    const fs = require('fs');
-    var filename = "index.html";
-    var htmldata = fs.readFile(filename,'utf8',(err,data) => {
-        try {
-            res.send(data);
-        }
-        catch(err){
-            res.send("404 Error");
-        };
-    });
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 
-})
+});
+app.get('/dailysub', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/public/submissions.html'));
+});
 
-app.get('/hi', (req, res) => {
-    res.send('Hello World!')
+const server = app.listen(port, () => {
+    console.log(`Canvas app listening on port ${port}`)
 })
+var io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+  io.sockets.on('connection', (socket) => {
+    console.log('Client connected: ' + socket.id)
+    socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
+    socket.on('disconnect', () => console.log('Client ' + socket.id + ' has disconnected'))
+   })
+
